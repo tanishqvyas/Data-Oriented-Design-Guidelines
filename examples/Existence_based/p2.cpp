89f71d8c5a93840456de8f70439d7950ecc2e32b
Rishit Chaudhary <rishit.c@live.com>
2021-05-06 01:58:14 +0530

    Updated skip count

--- examples/Existence_based/p2.cpp
+++ examples/Existence_based/p2.cpp
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
     AgentProcessor obj{N};
     std::srand(1);
 
-    for(std::size_t i = 0; i < N; i+=20) {
+    for(std::size_t i = 0; i < N; i+=2000) {
         obj.take_damage(obj.agent[i]);
     }
     // std::cout << "Here" << std::endl;
