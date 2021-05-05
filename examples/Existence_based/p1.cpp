2138bb85b2648357649c59947351d5f788bada79
Rishit Chaudhary <rishit.c@live.com>
2021-05-05 20:34:15 +0530

    Added collections for states pattern

---
+++ examples/Existence_based/p1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <cstddef>
+#include <cstdlib>
+
+
+struct Agent {
+    Agent() : points{100}, health{100}, x{0}, y{0}
+    {}
+
+    std::size_t points;
+    std::size_t health;
+    int x, y;
+};
+
+class AgentProcessor {
+    public:
+    AgentProcessor(std::size_t N) : damaged_agent{}, agent{N}
+    {}
+
+    void take_damage(Agent& agent) {
+        agent.health -= (std::rand() % 20) + 1;  // Min: 1, Max: 20
+        damaged_agent.push_back(&agent);
+    }
+
+    void regenerate_health() {
+        for (auto it = std::begin(damaged_agent); it != std::end(damaged_agent);) {
+            ++(*it)->health;
+            if ((*it) -> health >= 100)
+                it = damaged_agent.erase(it);
+            else
+                ++it;
+        }
+    }
+
+    std::vector<Agent*> damaged_agent;
+    std::vector<Agent> agent; 
+};
+
+// void regenerate_health(Agent& agents) {
+//     for (auto& agent : agents) {
+//         if (agent.health < 100)
+//             ++agent.health;
+//     }
+// }
+
+int main(int argc, char *argv[]) {
+    std::size_t N = std::atoi(argv[argc - 1]);
+    AgentProcessor obj{N};
+    std::srand(1);
+
+    for(std::size_t i = 0; i < N; i+=20) {
+        obj.take_damage(obj.agent[i]);
+    }
+
+    while(obj.damaged_agent.size())
+        obj.regenerate_health();
+
+    return 0;
+}
---
+++ examples/Existence_based/p2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <cstddef>
+#include <cstdlib>
+
+
+struct Agent {
+    Agent() : points{100}, health{100}, x{0}, y{0}
+    {}
+
+    std::size_t points;
+    std::size_t health;
+    int x, y;
+};
+
+class AgentProcessor {
+    public:
+    AgentProcessor(std::size_t N) : agent{N}, flag{true}
+    {}
+
+    void take_damage(Agent& agent) {
+        agent.health -= (std::rand() % 20) + 1;  // Min: 1, Max: 20
+    }
+
+    bool regenerate_health() {
+        flag = false;
+        for (auto it = std::begin(agent); it != std::end(agent); ++it) {
+            if (it -> health < 100) {
+                ++it->health;
+                flag = true;
+            }
+        }
+        // std::cout << std::boolalpha;
+        // std::cout << flag << std::endl;
+        return flag;
+    }
+
+    std::vector<Agent> agent; 
+    bool flag;
+};
+
+// void regenerate_health(Agent& agents) {
+//     for (auto& agent : agents) {
+//         if (agent.health < 100)
+//             ++agent.health;
+//     }
+// }
+
+int main(int argc, char *argv[]) {
+    std::size_t N = std::atoi(argv[argc - 1]);
+    AgentProcessor obj{N};
+    std::srand(1);
+
+    for(std::size_t i = 0; i < N; i+=20) {
+        obj.take_damage(obj.agent[i]);
+    }
+    // std::cout << "Here" << std::endl;
+
+    while(obj.regenerate_health());
+
+    return 0;
+}
