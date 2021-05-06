4a0582abd47db9d8a28071298bc3c0ddf4072cfd
tanishqvyas <tanishqvyas069@gmail.com>
2021-05-06 18:30:45 +0530

    -ve results for boolean members of class

---
+++ examples/Boolean_Boon_Or_Bane/MemberBool.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Player
+{
+
+private:
+public:
+    bool player_is_alive;
+    int player_health;
+    std::string player_name;
+    int player_power;
+    std::string player_id;
+    std::vector<int> kills;
+
+    Player()
+    {
+        player_is_alive = true;
+        player_health = 100;
+        player_name = "Raghav";
+        player_power = 100;
+        player_id = "kx22vv44ILOkjakjhef008292992";
+        kills = std::vector<int>(100, 100);
+    }
+
+    void decrease_health()
+    {
+        player_health -= (std::rand() % 40) + 1;
+        // player_health -=  10;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    std::srand(1);
+    int N = 1000000;
+
+    std::vector<Player> v(N, Player());
+
+    // Appraoch 1
+    bool flag = true;
+    while (flag)
+    {
+        flag = false;
+        for (int i = 0; i < N; i++)
+        {
+            if (v[i].player_is_alive)
+            {
+
+                v[i].decrease_health();
+                flag = true;
+
+                if (v[i].player_health <= 0)
+                {
+                    v[i].player_is_alive = false;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
