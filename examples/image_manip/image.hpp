dcae0505d3bd728a592391ccf8e74773cd00947f
Rishit Chaudhary <rishit.c@live.com>
2021-05-07 00:11:00 +0530

    Added image object implementation

---
+++ examples/image_manip/image.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <memory>
+
+#include "render_buffer.hpp"
+#include "data.hpp"
+
+class image
+{
+public:
+    image(const std::string &);
+    image(const image &);
+    void write_to_buffer(const data::colour_data &);
+    ~image();
+
+private:
+    std::shared_ptr<std::fstream> image_file_;
+    unsigned int height_;
+    unsigned int width_;
+};
