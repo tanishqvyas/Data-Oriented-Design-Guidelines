141bdc492682f78ef8d76b8a1d5ec4b1aeec9040
Rishit Chaudhary <rishit.c@live.com>
2021-05-07 00:11:32 +0530

    Added temporary image generator code

---
+++ examples/image_manip/Img/gen_data.py
@@ -0,0 +1,24 @@
+import random
+
+
+def pixel_value():
+    r_ = random.randint(0, 255)
+    g_ = random.randint(0, 255)
+    b_ = random.randint(0, 255)
+    return str(r_*65536 + g_*256 + b_)
+
+
+img = "img_"
+
+height_ = 4
+width_ = 4
+
+for i in range(10):
+    file_name_ = img + str(i) + ".csv"
+    with open(file_name_, "w") as fhandle:
+        fhandle.write(str(height_) + "," + str(width_) + "\n")
+        ele = [[pixel_value() for _ in range(width_)] for _ in range(height_)]
+        for i in range(4):
+            line = ",".join(ele[i])
+            line += "\n"
+            fhandle.write(line)
