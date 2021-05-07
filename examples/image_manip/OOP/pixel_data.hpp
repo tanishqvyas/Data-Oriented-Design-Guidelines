6f41a66f77d32aafc958380df4c77c0a23555cfd
Rishit Chaudhary <rishit.c@live.com>
2021-05-07 13:00:51 +0530

    Added include for type alias

---
+++ examples/image_manip/OOP/pixel_data.hpp
@@ -0,0 +1,6 @@
+#include <tuple>
+
+using colour_intensity_t = unsigned int;
+using pixel_colour_t = std::tuple<colour_intensity_t,
+                                  colour_intensity_t,
+                                  colour_intensity_t>;
