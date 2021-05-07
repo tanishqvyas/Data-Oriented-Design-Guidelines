edbceef7b0a949d91344751f4b412ebfbe5b4d84
Rishit Chaudhary <rishit.c@live.com>
2021-05-08 01:05:15 +0530

    Using array instead of vector

--- examples/image_manip/DoD/data.hpp
+++ examples/image_manip/DoD/data.hpp
@@ -1,27 +1,22 @@
 #pragma once
 
 #include <tuple>
-#include <vector>
+#include <array>
 #include <utility>
 #include <memory>
 
 namespace data
 {
+    using colour_intensity_t = unsigned int;
+    using pixel_colour_t = std::tuple<colour_intensity_t,
+                                      colour_intensity_t,
+                                      colour_intensity_t>;
+    template <std::size_t pixel_colour_buffer_size>
     class colour_data
     {
     public:
-        using colour_intensity_t = unsigned int;
-        using pixel_colour_t = std::tuple<colour_intensity_t,
-                                          colour_intensity_t,
-                                          colour_intensity_t>;
-        colour_data() : data_{nullptr}, free_pixel_index{}
+        colour_data() : data_{}, free_pixel_index{}
         {
-        }
-
-        void set_size(std::size_t size)
-        {
-            data_ = std::make_unique<pixel_colour_t[]>(size);
-            free_pixel_index = 0;
         }
 
         void insert(colour_intensity_t &r, colour_intensity_t &g, colour_intensity_t &b)
@@ -30,20 +25,14 @@ namespace data
             ++free_pixel_index;
         }
 
-        pixel_colour_t* get_nown_ptr()
+        pixel_colour_t *get_nown_ptr()
         {
-            return data_.get();
+            return data_.data();
         }
 
     private:
-        std::unique_ptr<pixel_colour_t[]> data_;
+        std::array<pixel_colour_t, pixel_colour_buffer_size> data_;
+        //std::unique_ptr<pixel_colour_t[]> data_;
         std::size_t free_pixel_index;
-    };
-
-    struct position_data
-    {
-        using position = unsigned int;
-        using pixel_position = std::pair<position, position>;
-        std::vector<pixel_position> data_;
     };
 } // namespace data
