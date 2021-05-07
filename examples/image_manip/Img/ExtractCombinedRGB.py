2ecf9cb0f46b92685a8cd3ae10a6ddae28a79fc1
tanishqvyas <tanishqvyas069@gmail.com>
2021-05-07 11:34:23 +0530

    Added RGB extraction code

---
+++ examples/image_manip/Img/ExtractCombinedRGB.py
@@ -0,0 +1,41 @@
+import os
+import cv2 
+import numpy as np  
+
+
+
+IMAGE_PATH = os.path.join("base_image.png")
+
+# from PIL import Image
+
+# im = Image.open(IMAGE_PATH) # Can be many different formats.
+# pix = im.load()
+# print(im.size)  # Get the width and hight of the image for iterating over
+# print(pix[x,y])  # Get the RGBA Value of the a pixel of an image
+# pix[x,y] = value  # Set the RGBA Value of the image (tuple)
+# im.save('alive_parrot.png')  # Save the modified pixels as .png
+
+
+img = cv2.imread(IMAGE_PATH)
+
+#images are converted to 1:1 dimensions
+img = cv2.resize(img,(64,64)).tolist()
+
+for x in range(64):
+    for y in range(64):
+        
+        # Inserted Rishit's Formula here
+        rgb_combined = img[y][x][1]
+        rgb_combined = (rgb_combined << 8) + img[y][x][2]
+        rgb_combined = (rgb_combined << 8) + img[y][x][0]
+
+        '''
+        # Reversal
+        red = (rgb_combined >> 16) & 0xFF;
+        green = (rgb_combined >> 8) & 0xFF;
+        blue = rgb_combined & 0xFF;
+        '''
+        print(rgb_combined, end=", ")
+    
+    print()
+        
